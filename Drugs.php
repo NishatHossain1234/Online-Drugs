2384b5e6a72b29feabc2a86d7347fa32fa9c6ddf
Nishat Taslim <arju1314@gmail.com>
2016-11-23 14:28:54 +0600

    First Commit

---
+++ Drugs.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Drugs an E-Commerce online Order Category Flat Bootstrap Responsive Website Template|</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Drugs Responsive web template, Bootstrap Web Templates, Android Compatible web template,Smartphone Compatible web template" />
+<script type="application/x-javascript"> addEventListener("load", function() 
+{ setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="http://css/bootstrap.css" rel='stylesheet' type='text/css' />
+
+
+
+
+
+</head>
+<body>
+<div class="banner">
+   	  <div class="container">
+   	  	<div class="header_top">
+   	  	   <div class="header_top_left">
+	  	      <div class="box_11"><a href="checkout.html">
+		      <h4> <p>Drugs: <span class="simpleCart_total"></span> 
+			  (<span id="simpleCart_quantity" class="simpleCart_quantity"></span> items)</p>
+			  <img src="C:\xampp\htdocs\drugs.png" alt=""/>
+			  <div class="clearfix"> </div></h4>
+		      </a></div>
+	          <p class="empty"><a href="javascript:;" class="simpleCart_empty">Empty Cart</a></p>
+	          <div class="clearfix"> </div>
+	       </div>
+           <div class="header_top_right">
+		  	 <div class="lang_list">
+				<select tabindex="4" class="dropdown">
+					<option value="" class="label" value="">Taka</option>
+				</select>
+			 </div>
+			 
+			 
+			 
+			 
+			</body>
+</html> 
